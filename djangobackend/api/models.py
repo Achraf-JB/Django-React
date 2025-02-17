093f074c2ee2446d36d3a40c576c45ee70c35ccd
achref <jarbouiachraf899@gmail.com>
2025-02-17 11:53:03 +0100

    test updated

--- djangobackend/api/models.py
+++ djangobackend/api/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Student(models.Model):
     stuname=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.stuname 
--- djangobackend/api/urls.py
+++ djangobackend/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('student/',views.StudentList.as_view()),
+    path('student/', views.StudentList.as_view(), name='student-list'), 
 ]
